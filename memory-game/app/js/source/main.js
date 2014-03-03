d30b12c7240cae0dde99461e9ee63a2aaf6152fe
JoeShep <joeshepmedia@gmail.com>
2014-03-03 17:32:38 -0600

    Refactor CSS and Jade; Add images teaser on 'play'

--- memory-game/app/js/source/main.js
+++ memory-game/app/js/source/main.js
@@ -13,7 +13,6 @@
   }
 
   function playMultiSound(s) {
-    //debugger;
     var soundFX = new Audio();
     soundFX.src = s.attr('src');
     soundFX.load();
@@ -25,38 +24,45 @@
       location.reload();
     } else {
       $('#counter').animate({opacity: 1}, 500);
-      var pics = [];
-      pics[0] = ['media/robin.jpg', 0];
-      pics[1] = ['media/barbie.jpg', 1];
-      pics[2] = ['media/batman.jpg', 0];
-      pics[3] = ['media/bert.jpg', 3];
-      pics[4] = ['media/blues1.jpg', 4];
-      pics[5] = ['media/blues2.jpg', 4];
-      pics[6] = ['media/c3po.jpg', 5];
-      pics[7] = ['media/r2d2.jpg', 5];
-      pics[8] = ['media/ricky.jpg', 6];
-      pics[9] = ['media/lucy.jpg', 6];
-      pics[10]= ['media/laurel.jpg', 7];
-      pics[11]= ['media/hardy.jpg', 7];
-      pics[12]= ['media/cheech.jpg', 8];
-      pics[13]= ['media/chong.jpg', 8];
-      pics[14]= ['media/muppet1.jpg', 9];
-      pics[15]= ['media/muppet2.jpg', 9];
-      pics[16]= ['media/john.jpg', 2];
-      pics[17]= ['media/paul.jpg', 2];
-      pics[18]= ['media/ken.jpg', 1];
-      pics[19]= ['media/ernie.jpg', 3];
+      var runCount = 0;
 
-      pics = _.shuffle(pics);
-
-      $('.content').each(function(){
-        $(this).append('<img data-id="' + pics[0][1] + '"src=' + pics[0][0] + '>');
-        pics.shift();
-      });
+      (function picLoop(){
+        runCount++;
+        if(runCount > 3) {
+          return;
+        }
+        $('.content').html('');
+        var pics = [];
+        pics[0] = ['media/robin.jpg', 0];
+        pics[1] = ['media/barbie.jpg', 1];
+        pics[2] = ['media/batman.jpg', 0];
+        pics[3] = ['media/bert.jpg', 3];
+        pics[4] = ['media/blues1.jpg', 4];
+        pics[5] = ['media/blues2.jpg', 4];
+        pics[6] = ['media/c3po.jpg', 5];
+        pics[7] = ['media/r2d2.jpg', 5];
+        pics[8] = ['media/ricky.jpg', 6];
+        pics[9] = ['media/lucy.jpg', 6];
+        pics[10]= ['media/laurel.jpg', 7];
+        pics[11]= ['media/hardy.jpg', 7];
+        pics[12]= ['media/cheech.jpg', 8];
+        pics[13]= ['media/chong.jpg', 8];
+        pics[14]= ['media/muppet1.jpg', 9];
+        pics[15]= ['media/muppet2.jpg', 9];
+        pics[16]= ['media/john.jpg', 2];
+        pics[17]= ['media/paul.jpg', 2];
+        pics[18]= ['media/ken.jpg', 1];
+        pics[19]= ['media/ernie.jpg', 3];
+        pics = _.shuffle(pics);
+        $('.content').each(function(){
+          $(this).append('<img data-id="' + pics[0][1] + '"src=' + pics[0][0] + '>').toggleClass('hidden', 500).delay(500).toggleClass('hidden', 500);
+          pics.shift();
+        });
+        setTimeout(picLoop, 1600);
+      })();
       $('.play').toggleClass('play reset').text('Reset');
     }
   }
-
 
   function flipIt(){
     var $this = $(this);
@@ -85,14 +91,13 @@
   }
 
   function checkMatch(){
-    //debugger;
     var $pic1 = $('.flipped:eq(0)').find('img').data('id');
     var $pic2 = $('.flipped:eq(1)').find('img').data('id');
     if( $pic1 === $pic2 ){
       $('.flipped').addClass('matched').removeClass('flipped');
       $('.matched').children().css('border','5px solid green');
       playMultiSound($('audio[id=a'+$pic1+']'));
-      $('#boardContainer').animate({'background-color':'green'},300).delay(300).animate({'background-color':'#993399'},200);
+      $('#boardContainer').animate({'background-color':'green'},300).delay(300).animate({'background-color':'#7792A8'},200);
       matches += 1;
       $('#match-count').text(' ' + matches);
       if($('.matched').length === 20){
